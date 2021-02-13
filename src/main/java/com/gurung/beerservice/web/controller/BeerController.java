bf9aa8ff1624bfbb34d2af894c7556161d717434
suraj <suraj@DESKTOP-DQS0LHE>
2021-02-13 12:00:46 -0600

    beer controller added

---
+++ src/main/java/com/gurung/beerservice/web/controller/BeerController.java
@@ -0,0 +1,32 @@
+package com.gurung.beerservice.web.controller;
+
+import com.gurung.beerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable(name = "beerId") UUID beerId){
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+//        TODO: add
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable(name = "beerId") UUID beerId,@RequestBody BeerDto beerDto){
+//        TODO: add
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+}
