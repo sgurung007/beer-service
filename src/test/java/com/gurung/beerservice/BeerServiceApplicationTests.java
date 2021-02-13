a94d23b8b3cbbd05fb47fe69d9f087117b83d635
suraj <suraj@DESKTOP-DQS0LHE>
2021-02-13 11:29:50 -0600

    Initial commit

---
+++ src/test/java/com/gurung/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurung.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
