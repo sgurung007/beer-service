018a923398a682f066b537fcb60ff2dada77c2e6
suraj <suraj@DESKTOP-DQS0LHE>
2021-02-14 11:37:56 -0600

    added beerStub as BeerLoader

---
+++ src/main/java/com/gurung/beerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,51 @@
+package com.gurung.beerservice.bootstrap;
+
+import com.gurung.beerservice.domain.Beer;
+import com.gurung.beerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private  final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+            loadBeerObject();
+    }
+
+    private void loadBeerObject() {
+        if(beerRepository.count() == 0){
+            beerRepository.save(Beer.builder()
+                .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(33701000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build()
+            );
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(33701000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build()
+            );
+
+
+//            System.out.println("loaded Beers: "+beerRepository.count());
+        }
+    }
+}
