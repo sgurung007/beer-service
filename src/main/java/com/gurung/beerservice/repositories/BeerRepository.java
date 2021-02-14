806687ff88139e97acfb1a75bd014fcf7063c0b1
suraj <suraj@DESKTOP-DQS0LHE>
2021-02-14 11:25:57 -0600

    adding BeerRepository

---
+++ src/main/java/com/gurung/beerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.gurung.beerservice.repositories;
+
+import com.gurung.beerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
